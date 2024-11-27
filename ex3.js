2f5cbb209380319ed71024d1d646a0b842b0ba1d
Eduardoesena <168350006+Eduardoesena@users.noreply.github.com>
2024-11-27 01:09:12 -0300

    Exercício dados na sala e comente

---
+++ ex3.js
@@ -0,0 +1,10 @@
+var idade=parseInt(prompt("Digite a sua idade"));
+var cnh= prompt("Você tem CNH?");
+var condicao=prompt("Você tem codições especiais?");
+
+if(((idade>=18)&(cnh=="sim"))||(condicao==1)){
+    alert("Você pode dirigir");
+
+}else{(alert("Deu ruim"));
+
+}
